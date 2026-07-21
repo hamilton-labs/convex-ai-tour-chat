47203b1e91213788761aa4177cc247c3e4a15cfa
Hamilton Labs <173300401+hamilton-labs@users.noreply.github.com>
2026-07-21 16:22:49 +0000

    Linked mutation and query to Convex remote DB to sync client and backend

---
+++ convex/chat.ts
@@ -0,0 +1,28 @@
+// Update your server import like this:
+import { query, mutation } from "./_generated/server";
+import { v } from "convex/values";
+
+// Add the following function to the file:
+export const getMessages = query({
+  args: {},
+  handler: async (ctx) => {
+    // Get most recent messages first
+    const messages = await ctx.db.query("messages").order("desc").take(50);
+    // Reverse the list so that it's in a chronological order.
+    return messages.reverse();
+  },
+});
+
+export const sendMessage = mutation({
+  args: {
+    user: v.string(),
+    body: v.string(),
+  },
+  handler: async (ctx, args) => {
+    console.log("This TypeScript function is running on the server.");
+    await ctx.db.insert("messages", {
+      user: args.user,
+      body: args.body,
+    });
+  },
+});
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
 import { useEffect, useState } from "react";
 import { faker } from "@faker-js/faker";
+import { useQuery, useMutation } from "convex/react";
+import { api } from "../convex/_generated/api";
 
 // For demo purposes. In a real app, you'd have real user data.
 const NAME = getOrSetFakeName();
 
 export default function App() {
-  const messages = [
-    { _id: "1", user: "Alice", body: "Good morning!" },
-    { _id: "2", user: NAME, body: "Beautiful sunrise today" },
-  ];
+  // Replace the `const messages = ...` line with the following
+  // const messages = [
+  //   { _id: "1", user: "Alice", body: "Good morning!" },
+  //   { _id: "2", user: NAME, body: "Beautiful sunrise today" },
+  // ];
+  const messages = useQuery(api.chat.getMessages);
   // TODO: Add mutation hook here.
+  const sendMessage = useMutation(api.chat.sendMessage);
 
   const [newMessageText, setNewMessageText] = useState("");
 
@@ -41,7 +46,8 @@ export default function App() {
       <form
         onSubmit={async (e) => {
           e.preventDefault();
-          alert("Mutation not implemented yet");
+          // alert("Mutation not implemented yet");
+          await sendMessage({ user: NAME, body: newMessageText });
           setNewMessageText("");
         }}
       >
